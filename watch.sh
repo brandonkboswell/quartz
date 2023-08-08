2b71812b4d56729e51475ac89e66475d022fff58
Brandon Boswell <brandonkboswell@gmail.com>
2023-08-07 22:33:26 -0400

    Updated Paths

--- watch.sh
+++ watch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-export GOPATH=/Users/$USER/go; export GOROOT=/usr/local/go; export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin"; cd ~/Sites/quartz; nodemon -w /Users/brandonkboswell/My\ Drive/2.\ Areas/Obsidian/bkb -w ~/Sites/quartz/assets/js -w ~/Sites/quartz/assets/styles -w ~/Sites/quartz/layouts -w ~/Sites/quartz/config.toml -w ~/Sites/quartz/data/config.yaml -x "~/Sites/quartz/compile.sh" -e md,html,js,scss,xml
+export GOPATH=/Users/$USER/go; export GOROOT=/opt/homebrew/opt/go/libexec; export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin"; cd ~/Sites/quartz; nodemon -w /Users/brandonkboswell/Library/Mobile Documents/iCloud~md~obsidian/Documents/bkb -w ~/Sites/quartz/assets/js -w ~/Sites/quartz/assets/styles -w ~/Sites/quartz/layouts -w ~/Sites/quartz/config.toml -w ~/Sites/quartz/data/config.yaml -x "~/Sites/quartz/compile.sh" -e md,html,js,scss,xml
