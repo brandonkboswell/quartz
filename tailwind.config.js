a68c7f9e1f33005515c5003772002c772727049f
Brandon Boswell <brandonkboswell@gmail.com>
2023-11-24 16:53:28 -0500

    Continued Tailwind Styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,12 +15,33 @@ module.exports = {
     }),
     require('@tailwindcss/typography'),
   ],
-  content: [
-    "./content/*.md",
-    "./layouts/**/*.html",
+  content: {
+    relative: true,
+    files: [
+      "./content/*.md",
+      "./layouts/**/*.html",
+    ],
+  },
+  safelist: [
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'contessa': {
+          '50': '#fbf6f5',
+          '100': '#f6ecea',
+          '200': '#f0dcd8',
+          '300': '#e4c3bd',
+          '400': '#d3a096',
+          '500': '#ba7264',
+          '600': '#aa6558',
+          '700': '#8e5347',
+          '800': '#77463d',
+          '900': '#643f38',
+          '950': '#351e1a',
+        },
+      }
+    },
   },
 }
 
